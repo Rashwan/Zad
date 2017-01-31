0bfe093754d745d9e3c52f255149467e3647e0c3
Rashwan <ahmed.t.rashwan@gmail.com>
2017-01-31 17:15:22 +0200

    remove unnecessary Facebook Sdk initialization

--- zad/src/main/java/com/app/zad/ZadApplication.java
+++ zad/src/main/java/com/app/zad/ZadApplication.java
@@ -2,8 +2,6 @@ package com.app.zad;
 
 import android.app.Application;
 
-import com.facebook.FacebookSdk;
-
 /**
  * Created by rashwan on 12/17/16.
  */
@@ -12,6 +10,5 @@ public class ZadApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FacebookSdk.sdkInitialize(this);
     }
 }
